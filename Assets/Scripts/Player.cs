90571429ae7973711e6c32f3ece0c661be4443a9
Adam <46858866+weenie-walker-games@users.noreply.github.com>
2021-10-02 00:04:53 -0400

    Created Third Party Character using video by Brackeys on YouTube

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ namespace WeenieWalker
     public class Player : MonoBehaviour
     {
 
+        [SerializeField] private float _moveSpeed = 3f;
+        [SerializeField] private float _jumpForce = 2f;
+
         private void OnEnable()
         {
             
@@ -15,6 +18,50 @@ namespace WeenieWalker
         private void OnDisable()
         {
             
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            //Check for input
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+            }
+
+
+
+
+
+            Move();
+
+        }
+
+        private void Move()
+        {
+
+            Vector3 moveVector;
+            moveVector.x = Input.GetAxis("Horizontal");
+            moveVector.z = Input.GetAxis("Vertical");
+            moveVector.y = 0;
+
+            Debug.Log("pre: " + moveVector);
+
+            moveVector.x = transform.position.x * (moveVector.x * _moveSpeed * Time.deltaTime);
+            moveVector.z = transform.position.z * (moveVector.x * _moveSpeed * Time.deltaTime);
+            moveVector.y = transform.position.y;
+            Debug.Log("post: " + moveVector);
+
+            //transform.position = moveVector;
+        }
+
+        private void Jump()
+        {
+
         }
     }
 }
