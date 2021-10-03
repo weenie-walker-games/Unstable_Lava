ead8cfd344a61151566550326c0f99f746ed752b
Adam <46858866+weenie-walker-games@users.noreply.github.com>
2021-10-03 11:00:45 -0400

    Started creating prefab variants of my blocks. Created the exit block with an event but no UI or game logic yet

---
+++ Assets/Scripts/ExitZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WeenieWalker
+{
+    public class ExitZone : MonoBehaviour
+    {
+
+        public static event System.Action OnPlayerReachExit;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                OnPlayerReachExit?.Invoke();
+                Debug.Log("Player reached exit");
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Interactions/Interacted Items/TreasureChest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WeenieWalker
+{
+    public class TreasureChest : InteractedItems
+    {
+        public static event System.Action<int> OnTreasureChestOpen;
+
+        [SerializeField] private Animator _anim;
+        [SerializeField] private int _treasureValue = 10;
+
+        public override void Interact()
+        {
+            _anim.SetTrigger("Interact");
+            OnTreasureChestOpen?.Invoke(_treasureValue);
+        }
+
+        public override void Reset()
+        {
+            //Does not reset
+        }
+    }
+}
